aa7e4c1d22159a438b8799d953c6ae21a96f453e
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2020-08-14 00:10:36 +0200

    Added more names

--- js/index.js
+++ js/index.js
@@ -6,7 +6,20 @@ window.onload = function () {
 		"Tobias Jungbluth",
 		"Tobias Jungblut",
 		"Tobias Jungbluth ohne h",
-		"Tobot"
+		"Tobot",
+		"Diggi",
+		"Dein Freund Tobi",
+		"Traubentobias",
+		"Bodenmensch",
+		"Tobi Tebi Taben Torben Torsten Hauptachsentransformation xXDoggolover96Xx Gram-Schmidt-Verfahren Penisdestroyer Singulärwertzerlegung",
+		"Bia",
+		"WeGlove",
+		"TPJ",
+		"Bili",
+		"Tobens",
+		"Juniorelf",
+		"die beste Soraka",
+		"der beste Jhin"
 	]
     document.getElementById('HEAD').innerHTML = "Hello, my name is " + names[Math.floor(Math.random()*names.length)];
 };
