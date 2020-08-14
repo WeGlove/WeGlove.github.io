a5eb748912f08d6cc67a7f377ab46afbf48da980
WeGlove <tobias.jungbluth.tj@gmail.com>
2020-08-14 12:04:31 +0200

    Made triangle

--- js/blog.js
+++ js/blog.js
@@ -2,5 +2,20 @@ var intervalID = window.setInterval(myCallback, 500);
 
 
 function myCallback() {
-    console.log("Test")
+    var svg = document.getElementById('Triangle');
+    var radius = 100;
+    var point_a = pointOnACircle(radius, radius);
+    var point_b = pointOnACircle(radius, radius);
+    var point_c = pointOnACircle(radius, radius);
+    console.log(point_a[0] + "," + point_a[1] + " " + point_b[0] + "," + point_b[1] + " " + point_c[0] + "," + point_c[1])
+    svg.setAttribute("points", point_a[0] + "," + point_a[1] + " " + point_b[0] + "," + point_b[1] + " " + point_c[0] + "," + point_c[1]);
+    console.log(svg);
+}
+
+function pointOnACircle(radius, offset=0) {
+    x = Math.random();
+    y = Math.random();
+    x_circle = Math.cos(x * Math.PI * 2) * radius + offset;
+    y_circle = Math.sin(y * Math.PI * 2) * radius + offset;
+    return [x_circle, y_circle];
 }
