ff2c2e27d814152a6e7772cb1f0cda60b4a522e6
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2020-08-16 14:13:03 +0200

    Moved Vector class into a different lib

--- js/bezier.js
+++ js/bezier.js
@@ -1,5 +1,34 @@
-var intervalID = window.setInterval(myCallback, 500);
+function preserve_knot(knot, end_point){
+	return knot.add(end_point.sub(knot).mul_scal(-2));
+}
 
 function myCallback() {
-    <path d="M10 10 C 20 20, 40 20, 50 10" stroke="black" fill="transparent"/>
-}
+    var svg = document.getElementById('curve');
+	var point_start = document.getElementById('point_start');
+	var point_end = document.getElementById('point_end');
+	var new_fourth_point = new Vector2D([Math.random()*size, Math.random()*size]);
+	first_point = fourth_point;
+	fourth_point = new_fourth_point;
+	second_point = preserve_knot(third_point, fourth_point);
+	third_point = new Vector2D([Math.random()*size, Math.random()*size]);
+    svg.setAttribute("d", "M" + first_point.values[0] + " " + first_point.values[1] + 
+					 " C "+ second_point.values[0] + " " + second_point.values[1] +", "+ 
+					 third_point.values[0] + " " + third_point.values[1] +", " +
+					 fourth_point.values[0] + " " + fourth_point.values[1]);
+	point_start.setAttribute("cx", first_point.values[0]);
+	point_start.setAttribute("cy", first_point.values[1]);
+	point_end.setAttribute("cx", fourth_point.values[0]);
+	point_end.setAttribute("cy", fourth_point.values[1]);
+	
+};
+
+
+
+var first_point = new Vector2D([0,0]);
+var second_point = new Vector2D([0,0]);
+var third_point = new Vector2D([0,0]);
+var fourth_point = new Vector2D([0,0]);
+var intervalID = window.setInterval(myCallback, 500);
+var size = 500;
+
+
