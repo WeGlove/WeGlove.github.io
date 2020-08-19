c8632f122469ca1d9fc91fb70cfab713bfd5e243
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2020-08-19 23:25:04 +0200

    Added Resources

--- js/index.js
+++ js/index.js
@@ -1,54 +1,6 @@
 window.onload = function () {
-	var names = [
-		"Zoni",
-		"DJ MC Hongkong Ferienwohnung",
-		"Tobi",
-		"Tobias Jungbluth",
-		"Tobias Jungblut",
-		"Tobias Jungbluth ohne h",
-		"Tobot",
-		"Diggi",
-		"Dein Freund Tobi",
-		"Traubentobias",
-		"Bodenmensch",
-		"Tobi Tebi Taben Torben Torsten Hauptachsentransformation xXDoggolover96Xx Gram-Schmidt-Verfahren Penisdestroyer Singulärwertzerlegung",
-		"Bia",
-		"WeGlove",
-		"TPJ",
-		"Bili",
-		"Tobens",
-		"Juniorelf",
-		"die beste Soraka",
-		"der beste Jhin"
-	];
-	var activites = [
-		"slapping",
-		"coding something",
-		"making triangles",
-		"admiring you",
-		"vomiting in some toilet",
-		"reading Papers",
-		"working",
-		"making memories",
-		"playing games",
-		"casting fireballs",
-		"petting my dog",
-		"doggoing",
-		".",
-		"on a Spongebob marathon",
-		"making \"art\"",
-		"talking about life",
-		"still mad at Jojo for that one time I was standing in front of Boudicca with my 3 missionaries when he declared war on them",
-		"playing bad games",
-		"protecting weak men",
-		"writing a highschool wrestling fanfiction",
-		"hosting a cultural exchange",
-		"coding this website",
-		"starting projects I am never going finish",
-		"showing the police my underwear",
-		"famous on YouTube",
-		"eating Zwieback"
-	];
+	var names = data[0]["Name_Msg"];
+	var activities = data[0]["Doing_Msg"];
     document.getElementById('WhoIAm').innerHTML = "Hello, my name is " + names[Math.floor(Math.random()*names.length)];
-	document.getElementById('WhatIAmDoing').innerHTML = "I am " + activites[Math.floor(Math.random()*activites.length)];
+	document.getElementById('WhatIAmDoing').innerHTML = "I am " + activities[Math.floor(Math.random()*activities.length)];
 };
