3c7df8deb1822acb8036f657463197067683348f
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2021-02-11 23:48:23 +0100

    Fixed table issue

--- js/experiments/table.js
+++ js/experiments/table.js
@@ -3,3 +3,12 @@ let table = new Table(table_html);
 table.set_shape([10,10]);
 table.print();
 table.save_to_cookie();
+
+function reshape(){
+    edit_x = document.getElementById("shape_x");
+    edit_y = document.getElementById("shape_y");
+
+    table.clear();
+    table.set_shape([parseInt(edit_x.value),parseInt(edit_y.value)]);
+    table.print();
+}
--- js/libs/table.js
+++ js/libs/table.js
@@ -13,6 +13,7 @@ class Table{
         this.shape = [0,0];
         this.has_header = false;
         this.matrix = null;
+        this.table.innerHTML = "";
     }
     
     set_shape(shape){
@@ -33,7 +34,7 @@ class Table{
         for (let x=0; x<this.shape[0]; x++){
             text += "<tr>";
             for (let y=0; y<this.shape[1]; y++){
-                 text += "<td id=\"table_" + x +"_" + y + "><input type='text' value='" + this.matrix.values[x][y] +"'/></td>"
+                 text += "<td id=\"table_" + x +"_" + y + "\"><input type='text' value='" + this.matrix.values[x][y] +"'/></td>"
             }
             text += "</tr>"
         }
@@ -47,6 +48,22 @@ class Table{
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 
+    getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+        }
+
     save_to_cookie(){
         this.setCookie("Data", JSON.stringify(this.to_json()) ,1);
     }
@@ -55,8 +72,11 @@ class Table{
         return {"matrix": this.matrix.toJSON(), "table_id":this.table.id};
     }
 
-    load_from_cookie(){
-
+    static load_from_cookie(){
+        let data = JSON.parse(getCookie("Data"));
+        let table = new Table(data["id"]);
+        table.matrix = data["matrix"];
+        table.shape = table.matrix.shape;
     }
 
 
