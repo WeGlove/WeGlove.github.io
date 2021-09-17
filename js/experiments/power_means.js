77e03fa01acb2bc827a3def4c98dd1f239b237cd
WeGlove <tobias.jungbluth.tj@gmail.com>
2021-09-17 15:10:05 +0200

    Made better power means

--- js/experiments/power_means.js
+++ js/experiments/power_means.js
@@ -1,4 +1,4 @@
-var figure = new Figure(document.getElementById("figure"),  new Matrix([[-1,-1],[1,1]]));
+var figure = new Figure(document.getElementById("figure"),  new Matrix([[-1,-1],[1,1]]), true);
 var power_input = document.getElementById("power");
 var amount_input = document.getElementById("amount");
 
@@ -11,7 +11,12 @@ function plot(){
     for (let i=0; i < amount; i++){
         let polar = new Polar(1, i/amount*2*Math.PI);
         let old_abs = polar.to_absolute(new Matrix([[0,0]]));
-        let radius = power_mean([old_abs.values[0][0], old_abs.values[0][1]], power);
+
+        let radius; 
+        if (document.getElementById('exp').checked)
+            radius = exp_mean([old_abs.values[0][0], old_abs.values[0][1]], document.getElementById('abs').checked);
+        else
+            radius = power_mean([old_abs.values[0][0], old_abs.values[0][1]], power, document.getElementById('abs').checked);
         let new_polar = new Polar(radius, i/amount*2*Math.PI);
         let abs = new_polar.to_absolute(new Matrix([[0,0]]));
         values.push([abs.values[0][0], abs.values[0][1]]);
@@ -20,15 +25,33 @@ function plot(){
     figure.plot_line(new Matrix(values));
 }
 
-function power_mean(values, power){
+function power_mean(values, power, abs){
     acc = (power == 0) ? 1 : 0;
     for (let val of values){
         if (power == 0){
-            acc *= Math.abs(val);
+            if (abs)
+                acc *= Math.abs(val);
+            else 
+                acc *= val;
         } else {
-            acc += Math.abs(val) ** power;
+            if (abs)
+                acc += Math.abs(val) ** power;
+            else
+                acc += val ** power;
         }
     }
     acc = (power == 0) ? acc ** (1/values.length) : ((acc/ values.length) ** (1/power));
+    return acc;
+}
+
+function exp_mean(values, abs){
+    acc = 0;
+    for (let val of values){
+        if (abs)
+            acc += Math.pow(Math.E, Math.abs(val));
+        else
+            acc += Math.pow(Math.E, val);
+    }
+    acc = Math.log(acc)/values.length;
     return acc;
 }
